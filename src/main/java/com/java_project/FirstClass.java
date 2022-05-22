6aadb7fbfce9b62605b5708159d561f77b131823
suraj Phuyal <surajs.phuyal@gmail.com>
2022-05-22 21:52:26 +0545

    create a simple test class

---
+++ src/main/java/com/java_project/FirstClass.java
@@ -0,0 +1,7 @@
+package com.java_project;
+
+public class FirstClass {
+	public String Sample() {
+		return "sample";
+	}
+}
---
+++ src/test/java/com/java_project/FirstClassTest.java
@@ -0,0 +1,14 @@
+package com.java_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
